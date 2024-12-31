3c6387991be6a9a47473ed8f8d885f28cd569f1b
Hassan Qazi <hassanqazids@gmail.com>
2025-01-01 03:50:54 +0500

    Add files via upload
    Multiple java problems for Beginners to build Logics

---
+++ Multiple JAVA problems/Multiple Java problems.java
@@ -0,0 +1,168 @@
+
+// area of circle //
+// public class Main {
+// public static void main (String [] args){
+// final double PI = 3.14;
+// double radius;
+// double area ;
+// radius = 3.5;
+// area = PI*radius*radius;
+// System.out.println("heyy"+area);
+
+// }
+// }
+
+// light
+// class Light {
+// public static void main(String args[]) {
+// int lightspeed = 3000000;
+// long days = 1000;
+// long seconds = days * 24 * 60 * 60;
+// long distance = lightspeed * seconds;
+// System.out.println("In " + days + " days light will travel about " + distance
+// + " miles.");
+// }
+// }
+
+// basic code
+// public class Main {
+
+// public static void main (String [] args){
+// int a = 23;
+// System.out.println(a);
+
+// }
+
+// }
+
+// public class Main{
+
+// public static void main (String [] args){
+
+// int a = 50;
+// String malik = "Huzefa";
+
+// System.out.println(a);
+// System.out.println(malik);
+
+// }
+
+//
+// public class Main {
+// public static void main (String [] args){
+// String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+// for (String i : cars) {
+// System.out.println(i);
+// }
+// }
+// }
+// public class Main {
+// public static void main(String[] args) {
+// float a = 3.1f;
+// float b = 3.2f;
+// System.out.println(a + b);
+// }
+// }
+
+// Write single JAVA statement to do each of the following:
+// a. Print the message "Enter two numbers".
+// b. Assign the sum of variables b and c to variable a.
+// c. Create a variable of Boolean type and initialize it with false.
+// d. Assign and cast a float variable “fvar” value into an int type “ivar”
+// value.
+// 10. Write single one line JAVA statement to do each of the following:
+// a. Declare variables ‘a’ and ‘b’ to be of type ‘float’.
+// b. Declare and initialize variable ‘average’ to be of type ‘double.
+// c. Set variable ‘a’ to ’1234.234’.
+// d. Set variable ‘b’ to ‘’ 1.3 x 103 ”.
+// e. Add variable ‘a’ and ‘b’ and divide their sum by 2 and assign the result
+// to variable ‘average’.
+// f.
+// Print "The average is: " followed by the value of variable ‘average’.
+
+// a. Assign and cast a float variable “fvar” value into an int type “ivar”
+// value.
+
+// public class Main {
+// public static void main (String [] args){
+// float fvar = 33.3f;
+// int ivar = (int) fvar;
+// System.out.println(ivar);
+// }
+// }
+
+// Declare variables ‘a’ and ‘b’ to be of type ‘float’.
+// Write single one line JAVA statement to do each of the following:
+// a. Declare variables ‘a’ and ‘b’ to be of type ‘float’.
+// b. Declare and initialize variable ‘average’ to be of type ‘double.
+// c. Set variable ‘a’ to ’1234.234’.
+// d. Set variable ‘b’ to ‘’ 1.3 x 103 ”.
+// e. Add variable ‘a’ and ‘b’ and divide their sum by 2 and assign the result
+// to variable ‘average’.
+// f.
+// Print "The average is: " followed by the value of variable ‘average’.
+// public class main {
+// public static void main(String[] args) {
+// for (int i = 1; i<10000; i++){
+// System.out.println(i);}
+// }
+// }
+//
+
+// Write a program which initialize the string object with value “I am string
+// object” then perform the following
+// operations:
+// a. Find the length of the string.
+// b. Find the position of letter ‘m’ in a string.
+// c. Convert the string into uppercase letters.
+// d. Create a new string from above string with the value of “string objec
+
+// b. Find the position of letter ‘m’ in a string.
+// public class Main {
+// public static void main(String[] args) {
+// String str = "i am a string";
+// int mynum = str.indexOf('m');
+// System.out.println( mynum);
+
+// }
+// }
+
+// // a. Find the length of the string.
+
+// public class Main {
+// public static void main(String[] args) {
+// String str = "i am a string";
+// int mynum = str.length();
+// System.out.println( mynum);
+
+// }
+// }
+
+// d. Create a new string with the value "string object"
+
+// public class Main {
+
+// public static void main(String[] args) {
+// String str = "hello doll";
+// String newString = str.substring(0);
+// System.out.println(newString);
+
+// }
+// }
+
+// // multi d with foreach
+// public class Main {
+// public static void main(String[] args) {
+// // 2D Array
+// int[][] array = {
+// {1, 2, 3, 4, 5},
+// {6, 7, 8, 9, 10},
+// };
+// for (int[] row : array) {
+// for (int num : row) {
+// System.out.print(num);
+// }
+// System.out.println(); //
+// }
+// }
+// }
